3f47edd0444b481c980d7399ab8575b916f6918c
Felipe Fernandes <fesousalocutor@gmail.com>
2022-07-21 02:40:45 -0300

    config: add husky and lint-staged

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const aaaa = 'help desk';
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
